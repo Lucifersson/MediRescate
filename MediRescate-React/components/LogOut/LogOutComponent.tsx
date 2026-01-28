c91f6f61042345639a10a5c2131d746b1dd0d37a
Hector <hectorbascuana@gmail.com>
2026-01-28 09:29:01 +0100

    botón logout

--- MediRescate-React/components/LogOut/LogOutComponent.tsx
+++ MediRescate-React/components/LogOut/LogOutComponent.tsx
@@ -1,21 +1,25 @@
+import { useAuthContext } from "@/core/context/UseAuthContext";
 import { useAuth } from "@/hooks/useAuth";
 import { Operario } from "@/types/types";
 import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
 import React from "react";
-import { Pressable } from "react-native";
+import { Pressable, Text } from "react-native";
 
 interface Props {
   onPress: () => void;
 }
 
 const LogOutComponent = ({ onPress }: Props) => {
-  // const { logOut } = useAuth()
+
+  const { user, logout } = useAuthContext()
 
   //NOTE: posible cambio de componente logout
   return (
-    <Pressable onPress={onPress}>
-      <Ionicons name="person-circle-outline" size={50} color="white" />
+    <Pressable onPress={logout} className="items-center">
+      <Ionicons name="person-circle-outline" size={50} className="text-white/80" />
+      <Text className="text-white/80 font-bold">Cerrar Sesión</Text>
+
     </Pressable>
   );
 };
