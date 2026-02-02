dc1ee2d9d14bf2a0efe503b00ad1260cacf33eec
Marcos <marcosmiquel77@gmail.com>
2026-02-02 11:52:46 +0100

    acabo de cometer un asesinato

--- Medirescate-Java/src/main/java/EmergencyHandler.java
+++ Medirescate-Java/src/main/java/EmergencyHandler.java
@@ -19,17 +19,13 @@ public class EmergencyHandler implements Runnable {
         this.inet = inet;
     }
 
-
     @Override
     public void run() {
 
         try (
-                Connection conn = DBConnectionManager.getInstance().getConnection();
-
                 BufferedReader in = new BufferedReader( //objeto para leer lo que ha llegado
                         new InputStreamReader(socket.getInputStream())
                 );
-
         ) {
             PrintWriter out = new PrintWriter( //objeto para enviar información
                     socket.getOutputStream(), true
@@ -38,29 +34,15 @@ public class EmergencyHandler implements Runnable {
             Request req = null;
             while ((line = in.readLine()) != null) {
                 System.out.println(AnsiColors.BLUE+"[EmergencyHandler "+inet+"]"+AnsiColors.RESET+" JSON recibido "+AnsiColors.GREEN_BRIGHT+"[<<] "+AnsiColors.RESET + line);
-
                 // Parseo JSON
                  req = gson.fromJson(line, Request.class);
-
             }
 
             String id = req.data.get("id").getAsString();
 
             OperariosManager.addOut(id, out);
 
-            while (!conn.isClosed()) {
-
-
-
-            }
-
-            // Procesar
-            Response resp = processRequestCode(req, conn);
-
-            System.out.println(AnsiColors.BLUE+"[EmergencyHandler "+inet+"]"+AnsiColors.RESET+" JSON respuesta "+AnsiColors.RED_BRIGHT+"[>>] "+ AnsiColors.RESET + gson.toJson(resp));
-
-            // Responder
-            out.println(gson.toJson(resp));
+            out.println(AnsiColors.BLUE+"[EmergencyHandler "+inet+"]"+AnsiColors.RESET+" JSON respuesta "+AnsiColors.RED_BRIGHT+"[>>] "+ AnsiColors.RESET + "{\"status\":\"success\",\"data\":{}");
 
         } catch (Exception e) {
             LogWriter.logError(e);
@@ -69,25 +51,6 @@ public class EmergencyHandler implements Runnable {
                 socket.close();
             } catch (IOException ignored) {}
         }
-
-        System.out.println(AnsiColors.BLUE+"[EmergencyHandler "+inet+"]"+AnsiColors.RED+" Conexión cerrada"+AnsiColors.RESET);
     }
 
-
-
-
-    private Response processRequestCode(Request req, Connection conn) {
-
-        return switch (req.code) {
-//            case "" -> {}
-
-
-            default -> {
-                LogWriter.logError(new Exception("[EmergencyHandler] - Codigo de operación no encontrado"));
-                yield new ResponseMSG("error", "[EmergencyHandler] - \"Codigo de operación no encontrado");
-            }
-        };
-
-
-    }
 }
--- Medirescate-Java/src/main/java/SecondaryServer.java
+++ Medirescate-Java/src/main/java/SecondaryServer.java
@@ -39,7 +39,6 @@ public class SecondaryServer {
             LogWriter.logError(e);
         }
     }
-
 }
 
 
