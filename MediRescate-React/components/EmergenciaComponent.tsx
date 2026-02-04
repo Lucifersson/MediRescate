29ecbdf19c3b236122edc707bd9805f543c7c03b
Lucifersson <mcastillol1906@gmail.com>
2026-02-04 14:13:11 +0100

    Cambio tipo emergencia

--- MediRescate-React/components/EmergenciaComponent.tsx
+++ MediRescate-React/components/EmergenciaComponent.tsx
@@ -1,31 +1,13 @@
-import { Emergencia } from "@/types/types";
+import { Emergencia, EmergenciaAdmin } from "@/types/types";
 import { Ionicons } from "@expo/vector-icons";
 import { Text, View } from "react-native";
 
 interface Props {
   // Ahora permitimos explícitamente que la emergencia sea nula o indefinida
-  emergencia: Emergencia | null | undefined;
+  emergencia: EmergenciaAdmin;
 }
 
 const EmergenciaComponent = ({ emergencia }: Props) => {
-  console.log("Valor de emergencia que llega al componente: ", emergencia);
-  // Renderizado de estado vacío/espera dentro del mismo estilo de tarjeta
-  if (emergencia === undefined || emergencia === null) {
-    return (
-      <View className="bg-white rounded-[40px] p-8 mx-6 shadow-xl shadow-black/5 border border-gray-100 items-center justify-center aspect-square">
-        <View className="bg-gray-50 p-6 rounded-full mb-4">
-          <Ionicons name="shield-checkmark-outline" size={60} color="#9ca3af" />
-        </View>
-        <Text className="text-gray-400 font-black text-xs uppercase tracking-[2px] mb-2 text-center">
-          Sistema en espera
-        </Text>
-        <Text className="text-lg font-bold text-gray-400 text-center px-4">
-          No hay emergencias activas en este momento
-        </Text>
-      </View>
-    );
-  }
-
   // Renderizado cuando SÍ hay una emergencia
   return (
     <View className="bg-white rounded-[40px] p-8 mx-6 shadow-xl shadow-black/10 border border-gray-100 items-center justify-center aspect-square">
@@ -54,7 +36,7 @@ const EmergenciaComponent = ({ emergencia }: Props) => {
         </Text>
         <View className="bg-gray-100 px-3 py-1 rounded-full mt-2">
           <Text className="text-[10px] font-black text-gray-500 uppercase">
-            ID Emergencia: {emergencia.id}
+            ID Operario: {emergencia.id_operario}
           </Text>
         </View>
       </View>
