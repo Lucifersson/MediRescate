44b85f169563e36ad55a5441e3370f03667ec1d1
Lucifersson <mcastillol1906@gmail.com>
2026-02-12 19:09:32 +0100

    Añadida version 1 de mapas

--- MediRescate-React/app/(stack)/(tabs)/admin/mapas/index.tsx
+++ MediRescate-React/app/(stack)/(tabs)/admin/mapas/index.tsx
@@ -1,12 +1,43 @@
-import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
-const AdminMapasScreen = () => {
+const MapScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>Mapas</Text>
+    <SafeAreaView style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        showsMyLocationButton
+        showsUserLocation
+        initialRegion={{
+          latitude: 39.459520762987665,
+          longitude: -0.47044131140655987,
+          latitudeDelta: 0,
+          longitudeDelta: 0,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 39.45919266033844,
+            longitude: -0.46964300466007,
+          }}
+          title="Guimarayz"
+          description="Lugar de buenos almuerzos"
+        />
+      </MapView>
     </SafeAreaView>
   );
 };
+export default MapScreen;
 
-export default AdminMapasScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+    //backgroundColor: 'red'
+  },
+});
