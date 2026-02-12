bbb1742647bb15363565d1620e604e6cb30e1889
Lucifersson <mcastillol1906@gmail.com>
2026-02-12 22:25:48 +0100

    Mapa funcional vamos cojones

--- MediRescate-React/app/(stack)/(tabs)/admin/mapas/index.tsx
+++ MediRescate-React/app/(stack)/(tabs)/admin/mapas/index.tsx
@@ -1,8 +1,25 @@
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import { useOperariosDisponibles } from "@/hooks/useOperariosDisponibles";
+import { useEffect } from "react";
 
 const MapScreen = () => {
+  const { operarios, solicitarOperarios } = useOperariosDisponibles();
+
+  useEffect(() => {
+    // Primera llamada inmediata al cargar la pantalla
+    solicitarOperarios();
+
+    // Configuramos el intervalo para ejecutar la llamada cada 5000ms
+    const interval = setInterval(() => {
+      console.log("Actualizando operarios...");
+      solicitarOperarios();
+    }, 5000);
+
+    // Limpieza: detenemos el intervalo cuando el usuario sale de la pantalla
+    return () => clearInterval(interval);
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <MapView
@@ -17,14 +34,27 @@ const MapScreen = () => {
           longitudeDelta: 0,
         }}
       >
-        <Marker
-          coordinate={{
-            latitude: 39.45919266033844,
-            longitude: -0.46964300466007,
-          }}
-          title="Guimarayz"
-          description="Lugar de buenos almuerzos"
-        />
+        {operarios.map((op) => {
+          // Convertimos explícitamente a número para evitar el error de la imagen
+          const lat = Number(op.latitud);
+          const lng = Number(op.longitud);
+
+          // Solo renderizamos si la conversión dio un número válido
+          if (!isNaN(lat) && !isNaN(lng)) {
+            return (
+              <Marker
+                key={op.id_operario}
+                coordinate={{
+                  latitude: lat,
+                  longitude: lng,
+                }}
+                title={op.nombre}
+                description={`ID: ${op.id_operario}`}
+              />
+            );
+          }
+          return null;
+        })}
       </MapView>
     </SafeAreaView>
   );
